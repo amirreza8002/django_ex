6be3db5e489e09d058215345151bcf27706c33c9
amirreza <amir.rsf1380@gmail.com>
2023-08-24 00:23:27 +0330

    pagination

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render, get_object_or_404
-
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.http import Http404
 from .models import Post
 
 
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get("page", 1)
+
+    try:
+        posts = paginator.page(page_number)
+    except (PageNotAnInteger, EmptyPage):
+        raise Http404
+
     return render(request, "blog/post/list.html", {"posts": posts})
 
 
