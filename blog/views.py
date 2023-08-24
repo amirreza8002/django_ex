00e125a92262f0ce3a4b455b5dfc26733d3bf4b7
amirreza <amir.rsf1380@gmail.com>
2023-08-24 15:31:16 +0330

    redefine list view as class based

--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,31 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import Http404
+from django.views.generic.list import ListView
+
 from .models import Post
 
 
-def post_list(request):
-    post_list = Post.published.all()
-    paginator = Paginator(post_list, 3)
-    page_number = request.GET.get("page", 1)
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = "posts"
+    paginate_by = 3
+    template_name = "blog/post/list.html"
 
-    try:
-        posts = paginator.page(page_number)
-    except (PageNotAnInteger, EmptyPage):
-        raise Http404
 
-    return render(request, "blog/post/list.html", {"posts": posts})
+post_list = PostListView.as_view()
+
+# def post_list(request):
+#     post_list = Post.published.all()
+#     paginator = Paginator(post_list, 3)
+#     page_number = request.GET.get("page", 1)
+#
+#     try:
+#         posts = paginator.page(page_number)
+#     except (PageNotAnInteger, EmptyPage):
+#         raise Http404
+#
+#     return render(request, "blog/post/list.html", {"posts": posts})
 
 
 def post_detail(request, year, month, day, post):
